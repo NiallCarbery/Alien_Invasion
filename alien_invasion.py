18d4aeba41e26674e93bbc2691797bc668a3bcda
NiallCarbery <niallcarbery0@gmail.com>
2023-07-10 19:46:14 +0100

    Revert "first commit"
    This reverts commit cb0795ced0760971f621ed6336cbd3139a2996ec.

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,19 +24,20 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._update_screen() 
 
     def _check_events(self):
-        # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+        """ Watch for keyboard and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Update images adn flip to new screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
