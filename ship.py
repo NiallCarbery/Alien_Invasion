5b19c3fc13952aaeffc49b9938e9342b3aab1c3e
NiallCarbery <niallcarbery0@gmail.com>
2023-07-10 20:25:54 +0100

    "Sets ship movemnt pased on variable speed before updating .rect position"

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
         """Initlaize ship and position"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settinfs = ai_game.settings
 
         #Load ship and rect
         self.image = pygame.image.load('images/ship.bmp')
@@ -14,17 +15,24 @@ class Ship:
         #Start ech new ship at bottom center
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #Store decimal value of ships horizontal position
+        self.x = float(self.rect.x)
+
         #Movement flag
         self.moving_right = False
         self.moving_left = False
     
     def update(self):
         """Update the ships position based on the movement flags"""
+        #Ipdate ships x value
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x += -1
-            
+            self.x -= self.settigs.ship_speed
+
+        #Update the rect position based on x
+        self.rect.x = self.x
+
     def blitme(self):
         """Draw tthe ship at its current location"""
         self.screen.blit(self.image, self.rect)
