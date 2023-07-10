c4b39d5b1880baa7cd589666d7a380ab4e42898d
NiallCarbery <niallcarbery0@gmail.com>
2023-07-10 19:17:23 +0100

    Inclusion of ship class and initlization in main()

---
+++ ship.py
@@ -0,0 +1,21 @@
+import pygame 
+
+class Ship:
+    """Class managing ship"""
+    def __init__(self, ai_game):
+        """Initlaize ship and position"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        #Load ship and rect
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        #Start ech new ship at bottom center
+        self.rect.midbottom = self.screen_rect.midbottom
+    
+    def blitme(self):
+        """Draw tthe ship at its current location"""
+        self.screen.blit(self.image, self.rect)
+
+    
