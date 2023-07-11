9feb121d5cab73d62f6513bb69ebaf73cac862aa
NiallCarbery <niallcarbery0@gmail.com>
2023-07-11 14:07:22 +0100

    Fix bug where multiple aliens are hit and only score once

--- alien_invasion.py
+++ alien_invasion.py
@@ -70,6 +70,7 @@ class AlienInvasion:
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -125,7 +126,8 @@ class AlienInvasion:
                 self.bullets, self.aliens, True, True)
 
         if collisions:
-            self.stats.score += self.settings.aliens_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
 
         if not self.aliens:
