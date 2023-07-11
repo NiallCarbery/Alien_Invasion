34aad009fac536b07de3a9702637ba3c81556da5
NiallCarbery <niallcarbery0@gmail.com>
2023-07-11 14:25:13 +0100

    Implemented Scoring and Game Icons

--- ship.py
+++ ship.py
@@ -1,11 +1,13 @@
 import pygame
-
-
-class Ship:
+ 
+from pygame.sprite import Sprite
+ 
+class Ship(Sprite):
     """A class to manage the ship."""
-
+ 
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
+        super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
@@ -17,17 +19,12 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
-        # Store a float for the ship's exact horizontal position.
+        # Store a decimal value for the ship's horizontal position.
         self.x = float(self.rect.x)
 
-        # Movement flags; start with a ship that's not moving.
+        # Movement flags
         self.moving_right = False
         self.moving_left = False
-
-    def center_ship(self):
-        """Center the ship on the screen."""
-        self.rect.midbottom = self.screen_rect.midbottom
-        self.x = float(self.rect.x)
 
     def update(self):
         """Update the ship's position based on movement flags."""
@@ -36,10 +33,15 @@ class Ship:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
-            
+
         # Update rect object from self.x.
         self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
