0e7c5bb9efee7386791467f5428addb4b4a2689d
NiallCarbery <niallcarbery0@gmail.com>
2023-07-11 13:27:51 +0100

    Comment updating

--- alien.py
+++ alien.py
@@ -1,8 +1,6 @@
 import pygame
-
 from pygame.sprite import Sprite
-
-
+ 
 class Alien(Sprite):
     """A class to represent a single alien in the fleet."""
 
@@ -26,9 +24,11 @@ class Alien(Sprite):
     def check_edges(self):
         """Return True if alien is at edge of screen."""
         screen_rect = self.screen.get_rect()
-        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
 
     def update(self):
         """Move the alien right or left."""
-        self.x += self.settings.alien_speed * self.settings.fleet_direction
+        self.x += (self.settings.alien_speed *
+                        self.settings.fleet_direction)
         self.rect.x = self.x
